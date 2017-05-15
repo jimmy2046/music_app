3f1da9171ccd0b3f4ced1f478fe9fa9c85fd5288
jimmy2046 <jimmychong_st@yahoo.com>
2017-05-14 17:26:36 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'    
+    
+# home_path
+  get '/home',     to: 'static_pages#home'
+
+# help_path    
+  get '/help',    to: 'static_pages#help'
+
+# about_path    
+  get '/about',    to: 'static_pages#about'    
+
+# contact_path
+  get '/contact',  to: 'static_pages#contact'    
+
+# signup_path    
+  get  '/signup',  to: 'users#new'
+        
 end
